549b21a263bb52f7494eac67708f1acc474a8a9b
Yatochi <aqildanial.17@gmail.com>
2024-11-29 11:56:20 +0800

    3rd commit

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -18,17 +18,47 @@ int main()
 
     drawBoard(spaces);
 
+    while (running){
+
+        playerMove(spaces, player);
+        drawBoard(spaces);
+    }
+    
+    
+
     return 0;
 }
 
 void drawBoard(char *spaces){
-
-    std::cout<< "     |     |     "<<'\n';
-    std::cout<< "  "<< spaces[0] <<"  |  "<< spaces[1] <<"  |  "<< spaces[2] <<"   "<<'\n';
-    std::cout<< "_____|_____|_____"<<'\n';
+    std::cout << '\n';
+    std::cout << "     |     |     "<<'\n';
+    std::cout << "  "<< spaces[0] <<"  |  "<< spaces[1] <<"  |  "<< spaces[2] <<"   "<<'\n';
+    std::cout << "_____|_____|_____"<<'\n';
+    std::cout << "     |     |     "<<'\n';
+    std::cout << "  "<< spaces[3] <<"  |  "<< spaces[4] <<"  |  "<< spaces[5] <<"   "<<'\n';
+    std::cout << "_____|_____|_____"<<'\n';
+    std::cout << "     |     |     "<<'\n';
+    std::cout << "  "<< spaces[6] <<"  |  "<< spaces[7] <<"  |  "<< spaces[8] <<"   "<<'\n';
+    std::cout << "     |     |     "<<'\n';
+    std::cout << '\n';
 }
 
 void playerMove(char *spaces, char player){
+    int number;
+    do
+    {
+        std::cout << "Enter a spot to place a marker (1-9)";
+        std::cin >> number;
+        number--;
+
+        if (spaces[number] == ' '){
+            spaces[number] == player;
+            break;
+        }
+        
+
+    } while (!number > 0 || !number < 8);
+    
 
 }
 
